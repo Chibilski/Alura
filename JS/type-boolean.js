f8b9575106f443633b598fa2fcb84f5c9a9bb63e
Luis Guilherme Camargo Merege <luis-merege@intranet.lojasmm.com.br>
2024-05-15 14:27:45 -0300

    Exemplos das aulas de java script

---
+++ JS/type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const text1 = "Luis"
+const text2 = "Guilherme"
+
+console.log(text1 === text2);
