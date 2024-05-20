fe0eeef968d453522c0f0d4362372182b79a0778
Luis Guilherme Camargo Merege <luis-merege@intranet.lojasmm.com.br>
2024-05-20 16:59:19 -0300

    códigos var e conversoés

---
+++ JS/conversoes.js
@@ -0,0 +1,10 @@
+//conversão implícita
+
+const numero = 456;
+const numeroString = Number("456a");
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+
---
+++ JS/truthy-falsy.js
@@ -0,0 +1,31 @@
+//boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+//undefined
+
+//null ==> Vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+console.log("------------------------------------------------------------")
+
+let numero = 3;
+let string = "Alura";
+
+console.log(typeof numero);
+console.log(typeof string);
+console.log("------------------------------------------------------------")
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
